6b8ef4d168a33dac760169d742930ee35ddf0304
yuuki1293 <yuuki1293@outlook.jp>
2021-10-09 15:50:05 +0900

    fizzbuzz

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
+import Control.Monad
 import Lib
 
 main :: IO ()
-main = print (1 + 2)
+main = do
+  fizzbuzz
+
+fizzbuzz = do
+  forM_ [1 .. 100] $ \x -> do
+    putStrLn $ prtFizzBuzz x
+
+isFizz :: Int -> Bool
+isFizz x = mod x 3 == 0
+
+isBuzz :: Int -> Bool
+isBuzz x = mod x 5 == 0
+
+isFizzBuzz :: Int -> Bool
+isFizzBuzz x = isFizz x && isBuzz x
+
+prtFizzBuzz :: Int -> [Char]
+prtFizzBuzz x = do
+  if isFizzBuzz x
+    then "FuzzBuzz"
+    else
+      if isFizz x
+        then "Fizz"
+        else if isBuzz x then "Buzz" else show x
